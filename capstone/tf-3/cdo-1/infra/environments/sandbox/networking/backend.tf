a04b6c3816f8677152237f66f7f40cda9a96744f
tutruong <tct31082004@gmail.com>
2026-06-27 21:59:02 +0700

    setup: configure remote s3 state backends for all environments and bootstrap

--- capstone/tf-3/cdo-1/infra/environments/sandbox/networking/backend.tf
+++ capstone/tf-3/cdo-1/infra/environments/sandbox/networking/backend.tf
@@ -1,7 +1,9 @@
-# 🌐 TODO: Cấu hình Backend State riêng biệt cho Networking
-# Key: "sandbox/networking/terraform.tfstate"
 terraform {
   backend "s3" {
-    # Tự động import bucket & lock table từ bootstrap
+    bucket         = "tf3-cdo1-sandbox-tfstate-474013238625"
+    key            = "sandbox/networking/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf3-cdo1-sandbox-tfstate-lock"
+    encrypt        = true
   }
 }
